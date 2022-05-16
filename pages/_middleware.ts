5d6ccefab74a4e90441dc04b2d66345f6092456f
Jacob Ford <j@cobford.com>
2022-05-15 22:31:49 -0400

    Log incoming requests

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -2,5 +2,6 @@ import type { NextRequest } from 'next/server'
 import redirects from '@lib/redirects'
 
 export async function middleware(req: NextRequest) {
+  console.info('Incoming request for', req.nextUrl.hostname)
   return await redirects(req)
 }
