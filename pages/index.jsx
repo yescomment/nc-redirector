c8ac533145dbbf585edbaae60320ebdfc51cd565
Jacob Ford <j@cobford.com>
2022-07-11 09:29:24 -0400

    Link to company site

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,7 +7,7 @@ export default function TableOfContents({ redirects }) {
       <Head>
         <title>Redirects of Jacob Ford</title>
       </Head>
-      <p><a href="//jacobford.com">Jacob Ford</a> buys funny little domains and sends them to relevant little places.</p>
+      <p><a href="//jacobford.com">Jacob Ford</a> buys funny little domains and sends them to relevant little places, for <a href="//nocomment.llc">business</a> + pleasure.</p>
       <p>His favorites are when he snags one from the background of fiction.</p>
       <ul>
       {Object.keys(redirects).map(domain =>
