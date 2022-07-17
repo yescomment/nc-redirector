6391f9f72992a467a62523e046eb445eccf0b5c6
Jacob Ford <j@cobford.com>
2022-07-17 12:45:58 -0400

    Log redirect events to Axiom

--- lib/redirects.ts
+++ lib/redirects.ts
@@ -1,6 +1,7 @@
 // eslint-disable-next-line @next/next/no-server-import-in-page
 import { NextRequest, NextResponse } from 'next/server'
 import parseTpl from './parse-template-literal';
+import Client, { datasets } from '@axiomhq/axiom-node';
 // import { upstashEdge } from './upstash'
 const redirectsJson = require('/redirects.json')
 
@@ -22,6 +23,12 @@ export default async function redirects(req: NextRequest) {
         // add more parsable replacements here
       })
     }
+    // log to Axiom
+    Client.datasets.ingest('nc-redirector',{
+      request: req,
+      from: req.url,
+      to: localRedirect.destination
+    })
     console.log(`Request for ${req.url} redirected to ${localRedirect.destination}`)
     return NextResponse.redirect(localRedirect.destination)
   } else {
