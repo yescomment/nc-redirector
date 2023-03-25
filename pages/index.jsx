d40f600d164942c915ad7331430fa4acc8fe7e53
Jacob Ford <j@cobford.com>
2023-03-25 19:36:37 -0400

    Update collection description

--- pages/index.jsx
+++ pages/index.jsx
@@ -7,8 +7,8 @@ export default function TableOfContents({ redirects }) {
       <Head>
         <title>Redirects of Jacob Ford</title>
       </Head>
-      <p><a href="//jacobford.com">Jacob Ford</a> buys funny little domains and sends them to relevant little places, for <a href="//nocomment.llc">business</a> + pleasure.</p>
-      <p>His favorites are when he snags one from the background of fiction.</p>
+      <p>I collect domains, for <a href="//nocomment.llc">business</a> + pleasure.</p>
+      <p>Examples include: fun new TLDs (.museum), ancient and forgotten campaigns (owlcitygalaxy.com), and sniping from the background of fiction (higginsgear.com, chirpeo.com), and pranks.</p>
       <ul>
       {Object.keys(redirects).map(domain =>
         <li key={domain}><a href={`//${domain}`}>{domain}</a></li>
